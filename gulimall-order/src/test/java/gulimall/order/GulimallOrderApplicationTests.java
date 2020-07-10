8edfedf40e977ce975bb557b9a287d1495694a19
SunQiXin <15153869872@163.com>
2020-07-10 15:26:23 +0800

    完成项目的初始化工作

---
+++ gulimall-order/src/test/java/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
