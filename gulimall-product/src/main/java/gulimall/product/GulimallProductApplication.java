4793f095dd7001be5eab4e50f6ee4d0d4a54759f
SunQiXin <15153869872@163.com>
2020-07-28 16:19:50 +0800

    使用SpringCache做缓存

--- gulimall-product/src/main/java/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/gulimall/product/GulimallProductApplication.java
@@ -2,6 +2,7 @@ package gulimall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
